3c3a8c850dba06ffc5d4f5a2e02ae807456555d2
Colton B <coltonbailiff@yahoo.com>
2023-04-24 20:39:53 -0400

    Updated Food and ShoppingList

--- src/Food/Food.java
+++ src/Food/Food.java
@@ -1,4 +1,5 @@
 package Food;
+import java.util.Scanner;
 
 public class Food {
 
@@ -18,15 +19,80 @@ public class Food {
         this.carbohydratesPerUnit = carbohydratesPerUnit;
     }
 
+    // TODO: Replace "name != name" with a variable that is stored somewhere else to ensure there's no duplicate food
+    // Like so; "name != foodDB.name"
     public void eat() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Select existing food: ");
+        name = input.nextLine();
 
+        if (name != name) {
+            System.out.println(name + " doesn't exist!");
+        }
+        else {
+            System.out.print("Update " + name + "'s calories per unit: ");
+            caloriesPerUnit = input.nextInt();
+
+            System.out.print("Update " + name + "'s fat per unit: ");
+            fatPerUnit = input.nextInt();
+
+            System.out.print("Update " + name + "'s protein per unit: ");
+            proteinPerUnit = input.nextInt();
+
+            System.out.print("Update " + name + "'s fiber per unit: ");
+            fiberPerUnit = input.nextInt();
+
+            System.out.print("Update " + name + " carbohydrates per unit: ");
+            carbohydratesPerUnit = input.nextInt();
+
+            System.out.println("You ate " + name + "!");
+        }
     }
 
     public void delete() {
+        while(true) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Select existing food (Or type exit to quit this option): ");
+            name = input.nextLine();
 
+            if (name != name) {
+                System.out.println("This food you're trying to delete doesn't exist. Please try again later.");
+            }
+            else if (name == "quit") {
+                break;
+            }
+            else {
+                System.out.println(name + " is deleted!");
+                break;
+            }
+        }
     }
 
     public void add() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter name of food: ");
+        name = input.nextLine();
 
+        if (name != name) {
+            System.out.print("Enter " + name + "'s calories per unit: ");
+            caloriesPerUnit = input.nextInt();
+
+            System.out.print("Enter " + name + "'s fat per unit: ");
+            fatPerUnit = input.nextInt();
+
+            System.out.print("Enter " + name + "'s protein per unit: ");
+            proteinPerUnit = input.nextInt();
+
+            System.out.print("Enter " + name + "'s fiber per unit: ");
+            fiberPerUnit = input.nextInt();
+
+            System.out.print("Enter " + name + " carbohydrates per unit: ");
+            carbohydratesPerUnit = input.nextInt();
+
+            System.out.println("New food added!");
+        }
+        else {
+            System.out.println(name + " already exist!");
+        }
     }
 }
--- src/Food/ShoppingList.java
+++ src/Food/ShoppingList.java
@@ -1,5 +1,83 @@
 package Food;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class ShoppingList {
+    Scanner input = new Scanner(System.in);
+    Map<String, List<String>> list = new HashMap<String, List<String>>();
+    List<String> foodList;
 
+    public Boolean validateList(String name) {
+        if (!list.containsKey(name)) {
+            System.out.println(name + " doesn't exist!");
+            return false;
+        }
+        return true;
+    }
+    
+    public void createList(String name) {
+        if (validateList(name) == true) {
+            foodList = new ArrayList<String>();
+
+            while(true) {
+                System.out.print("Add a food to the list (When done, type save): ");
+                foodList.add(input.nextLine());
+
+                if (input.nextLine() == "save") {
+                    list.put(name, foodList);
+                    break;
+                }
+            }
+            System.out.println(name + " list created!");
+        }
+    }
+
+    public void displayList(String name) {
+        if (validateList(name) == true) {
+            for (Map.Entry<String, List<String>> entry: list.entrySet()) {
+                System.out.println("List name: " + entry.getKey() + " -----");
+                System.out.println(entry.getValue());
+            }
+        }
+    }
+
+    public void editList(String name) {
+        int selectedItem;
+        String modifyFoodName;
+
+        if (validateList(name) == true) {
+            while(true) {
+                System.out.println("Select food to modify (Format: 'list number, food name'. When done, type save)");
+                System.out.print("Select a item of " + name + ": ");
+                selectedItem = input.nextInt();
+
+                System.out.print(selectedItem + " is selected. Modify food name (To remove this item, type delete): ");
+                modifyFoodName = input.nextLine();
+
+                if (modifyFoodName == "delete") {
+                    foodList.remove(selectedItem);
+                    System.out.print(selectedItem + " is removed from " + name + "!");
+                }
+                else {
+                    foodList.set(selectedItem, modifyFoodName);
+                }
+
+                if (input.nextLine() == "save") {
+                    list.put(name, foodList);
+                    break;
+                }
+            }
+            System.out.print(name + " list updated!");
+        }
+    }
+
+    public void deleteList(String name) {
+        if (validateList(name) == true) {
+            list.remove(name);
+            System.out.println(name + " list is deleted!");
+        }
+    }
 }
